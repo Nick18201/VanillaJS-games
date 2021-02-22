57bbd5c405d8c67f2d4cace64010bb3769c78c3f
nicolas blum <nblumferracci@gmail.com>
2021-02-22 13:05:54 +0100

    finish caracter action for up right jump and down right

--- index2.js
+++ index2.js
@@ -8,35 +8,46 @@ const images = {};
 images.player = new Image();
 images.player.src = "Cuphead.png";
 
-const playerWidth = images.player.width / 16;
-const playerHeight = images.player.height / 8;
-let playerFrameX = 3;
-let playerFrameY = 3;
-let playerX = 0;
-let playerY = 0;
-const playerSpeed = 6;
-
 const characterActions = [
   "up",
-  "top right",
+  // "top right",
   "right",
   "down right",
-  "down",
+  // "down",
   "jump",
 ];
 const characters = [];
+const numberOfCharacters = 10;
 
 class Character {
   constructor() {
     this.width = images.player.width / 16;
     this.height = images.player.height / 8;
     this.frameX = 3;
-    this.frameY = 3;
-    this.x = 0;
-    this.y = 0;
+    this.x = Math.random() * canvas.height;
+    this.y = Math.random() * canvas.height;
     this.speed = Math.random() * 1.5 + 3.5;
-    this.action = "right";
+    this.action =
+      characterActions[Math.floor(Math.random() * characterActions.length)];
+    if (this.action === "up") {
+      this.frameY = 0;
+      this.minFrame = 4;
+      this.maxFrame = 15;
+    } else if (this.action === "right") {
+      this.frameY = 3;
+      this.minFrame = 3;
+      this.maxFrame = 13;
+    } else if (this.action === "jump") {
+      this.frameY = 7;
+      this.minFrame = 0;
+      this.maxFrame = 9;
+    } else if (this.action === "down right") {
+      this.frameY = 4;
+      this.minFrame = 4;
+      this.maxFrame = 15;
+    }
   }
+
   draw() {
     drawSprite(
       images.player,
@@ -50,28 +61,54 @@ class Character {
       this.height
     );
     // animation of sprites
-    if (this.frameX < 13) this.frameX++;
-    else this.frameX = 3;
+    if (this.frameX < this.maxFrame) this.frameX++;
+    else this.frameX = this.minFrame;
   }
   update() {
     if (this.action === "right") {
-      if (this.x < canvas.width + this.width) this.x += this.speed;
-      else this.x = 0 - this.width;
+      if (this.x > canvas.width + this.width) {
+        this.x = 0 - this.width;
+        this.y = Math.random() * (canvas.height - this.height);
+      } else {
+        this.x += this.speed;
+      }
+    } else if (this.action === "up") {
+      if (this.y < 0 - this.height) {
+        this.y = canvas.height + this.height;
+        this.x = Math.random() * canvas.width;
+      } else {
+        this.y -= this.speed;
+      }
+    } else if (this.action === "down right") {
+      if (
+        this.y > canvas.height + this.height &&
+        this.x > canvas.width + this.width
+      ) {
+        this.y = 0 - this.height;
+        this.x = (Math.random() * canvas.width) / 2;
+      } else {
+        this.y += this.speed;
+        this.x += this.speed;
+      }
     }
   }
 }
-characters.push(new Character());
+for (i = 0; i < numberOfCharacters; i++) {
+  characters.push(new Character());
+}
 
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  characters[0].draw();
-  characters[0].update();
+  for (i = 0; i < characters.length; i++) {
+    characters[i].draw();
+    characters[i].update();
+  }
 }
 
-window.onload = setInterval(animate, 1000 / 30);
+window.onload = setInterval(animate, 1000 / 20);
 
 window.addEventListener("resize", function () {
   canvas.height = window.innerHeight;
