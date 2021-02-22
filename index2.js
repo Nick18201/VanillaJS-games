76a8cc5f4b50c73dd92bcf174d20411948635c39
nicolas blum <nblumferracci@gmail.com>
2021-02-22 10:56:18 +0100

    set animation for character with class inheritance

--- index2.js
+++ index2.js
@@ -16,20 +16,64 @@ let playerX = 0;
 let playerY = 0;
 const playerSpeed = 6;
 
+const characterActions = [
+  "up",
+  "top right",
+  "right",
+  "down right",
+  "down",
+  "jump",
+];
+const characters = [];
+
+class Character {
+  constructor() {
+    this.width = images.player.width / 16;
+    this.height = images.player.height / 8;
+    this.frameX = 3;
+    this.frameY = 3;
+    this.x = 0;
+    this.y = 0;
+    this.speed = Math.random() * 1.5 + 3.5;
+    this.action = "right";
+  }
+  draw() {
+    drawSprite(
+      images.player,
+      this.width * this.frameX,
+      this.height * this.frameY,
+      this.width,
+      this.height,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
+    // animation of sprites
+    if (this.frameX < 13) this.frameX++;
+    else this.frameX = 3;
+  }
+  update() {
+    if (this.action === "right") {
+      if (this.x < canvas.width + this.width) this.x += this.speed;
+      else this.x = 0 - this.width;
+    }
+  }
+}
+characters.push(new Character());
+
 function drawSprite(img, sX, sY, sW, sH, dX, dY, dW, dH) {
   ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawSprite(
-    images.player,
-    playerWidth * playerFrameX,
-    playerHeight * playerFrameY,
-    playerWidth,
-    playerHeight,
-    playerX,
-    playerY,
-    playerWidth,
-    playerHeight
-  );
+  characters[0].draw();
+  characters[0].update();
 }
+
+window.onload = setInterval(animate, 1000 / 30);
+
+window.addEventListener("resize", function () {
+  canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+});
